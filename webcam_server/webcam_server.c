de7d7edcf9e32dd3e0b27f4b5b991db39edb2399
timonkun <timonkun@VirtualBox.(none)>
2013-05-30 17:14:39 +0800

    update code for webcam server.

--- webcam_server/webcam_server.c
+++ webcam_server/webcam_server.c
@@ -4,45 +4,56 @@
 #include <unistd.h>
 #include <string.h>
 
-#define URL_PATH  "kaffeel.org/timeonkun/address.txt"
 #define CONFIG_FILE "address.txt"
-
+#define URL_PATH  "kaffeel.org/timeonkun/address.txt"
 
 typedef struct{
 	char pKeyword[40];
 	int  iItemNum;
-} keyword_struct;
+} Keyword_Struct;
 
 typedef struct{
-	int iCtrlValue;
-} control_struct;
+	int iCtrl_webcam;
+	int iCtrl_fan;
+	int iCtrl_feed;
+} Control_Struct;
 
+enum ctrl_code {
+CTRL_CLOSE,  //0
+CTRL_OPEN,
+
+};
 
 enum ctrl_mode {
-MODE_WEBCAM,
+MODE_WEBCAM,  //0
 MODE_FAN,
 MODE_FEED,
 MODE_MAX,
 };
 
-control_struct cur_ctrl[MODE_MAX] = {
-0,	// MODE_WEBCAM 
-0,	// MODE_FAN
-0,	// MODE_FEED
+Control_Struct cur_ctrl = {
+0,	// iCtrl_webcam 
+0,	// iCtrl_fan
+0,	// iCtrl_feed
 };
 
-control_struct last_ctrl[MODE_MAX] = {
+Control_Struct last_ctrl = {
 	0, 0, 0,
 };
-static keyword_struct config_keyword[] = {
+
+static Keyword_Struct config_keyword[] = {
 {"<CTRL_MODE_WEBCAM>", 1},  //0
 {"<CTRL_MODE_FAN>",    1},
 {"<CTRL_MODE_FEED>",   1},
 };
 
-int compare_update();
-int load_config_data(control_struct tmp_ctrl[]);
+int compare_update(void);
+int load_config_data(Control_Struct *tmp_ctrl);
 void download_config_file(void);
+
+int webcam_operation(int ctrl_code);
+int fan_operation(int ctrl_code);
+int feed_operation(int ctrl_code);
 
 int main(int argc, char **argv)
 {
@@ -77,9 +88,9 @@ void download_config_file(void)
 	}
 }
 
-int load_config_data(control_struct tmp_ctrl[])
+int load_config_data(Control_Struct *tmp_ctrl)
 {
-	int i, j, string_len;
+	int i, index, string_len;
 	char string[256];
 	char str_value[16];
 	int  ctrl_value;
@@ -94,16 +105,30 @@ int load_config_data(control_struct tmp_ctrl[])
 
 	while(fgets(string, 256, fp))
 	{
-		for(j = MODE_WEBCAM; j < MODE_MAX; j++)
+		for(index = MODE_WEBCAM; index < MODE_MAX; index++)
 		{
-			string_len = strlen(config_keyword[j].pKeyword);
-			if(0 == strncmp(config_keyword[j].pKeyword, string, string_len))
+			string_len = strlen(config_keyword[index].pKeyword);
+			if(0 == strncmp(config_keyword[index].pKeyword, string, string_len))
 			{
 				if(NULL != fgets(str_value, 16, fp))
 				{
-					cur_ctrl[j].iCtrlValue = atoi(str_value);
-					printf("%s: %d\n%d\n", string, \
-						config_keyword[j].iItemNum, cur_ctrl[j].iCtrlValue);
+					switch(index)
+					{
+						case MODE_WEBCAM:
+							tmp_ctrl->iCtrl_webcam = atoi(str_value);
+							break;
+						case MODE_FAN:
+							tmp_ctrl->iCtrl_fan = atoi(str_value);
+							break;
+						case MODE_FEED:
+							tmp_ctrl->iCtrl_feed = atoi(str_value);
+							break;
+						default:
+							printf("[%s] Error. index=%d", __func__, index);
+							exit(1);
+					}
+					//printf("%s: %d\n%d\n", string, \
+						config_keyword[index].iItemNum, atoi(str_value));
 				}
 				else
 				{
@@ -122,8 +147,55 @@ int load_config_data(control_struct tmp_ctrl[])
 	return 0;
 }
 
-// read config file, compare the ctrl values.
-int compare_update()
+int webcam_operation(int ctrl_code)
 {
-	load_config_data(cur_ctrl);
+	printf("%s: ctrl_code=%d\n", __func__, ctrl_code);
+	return 0;
+}
+
+int fan_operation(int ctrl_code)
+{
+
+	printf("%s: ctrl_code=%d\n", __func__, ctrl_code);
+	return 0;
+}
+
+int feed_operation(int ctrl_code)
+{
+	printf("%s: ctrl_code=%d\n", __func__, ctrl_code);
+	return 0;
+}
+
+// read config file, compare the ctrl values.
+int compare_update(void)
+{
+	load_config_data(&cur_ctrl);
+	//printf("webcam=%d, fan=%d, feed=%d\n", cur_ctrl.iCtrl_webcam, \
+			cur_ctrl.iCtrl_fan, cur_ctrl.iCtrl_feed);
+
+	if(cur_ctrl.iCtrl_webcam != last_ctrl.iCtrl_webcam)
+	{
+		//printf("last_ctrl.webcam=%d, cur_ctrl.webcam=%d\n", \
+				last_ctrl.iCtrl_webcam, cur_ctrl.iCtrl_webcam);
+		webcam_operation(cur_ctrl.iCtrl_webcam);
+		last_ctrl.iCtrl_webcam = cur_ctrl.iCtrl_webcam;
+	}
+
+	if(cur_ctrl.iCtrl_fan != last_ctrl.iCtrl_fan)
+	{
+		//printf("last_ctrl.fan=%d, cur_ctrl.fan=%d\n", \
+				last_ctrl.iCtrl_fan, cur_ctrl.iCtrl_fan);
+		fan_operation(cur_ctrl.iCtrl_fan);
+		last_ctrl.iCtrl_fan = cur_ctrl.iCtrl_fan;
+	}
+
+	if(cur_ctrl.iCtrl_feed != last_ctrl.iCtrl_feed)
+	{
+		//printf("last_ctrl.feed=%d, cur_ctrl.feed=%d\n", \
+				last_ctrl.iCtrl_feed, cur_ctrl.iCtrl_feed);
+		feed_operation(cur_ctrl.iCtrl_feed);
+		last_ctrl.iCtrl_feed = cur_ctrl.iCtrl_feed;
+	}
+
+	return 0;
 }
