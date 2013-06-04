4e845d2ed367737bd4f6565a7e46d4f77dfeb05c
timonkun <timonkun@VirtualBox.(none)>
2013-06-04 15:47:50 +0800

    add live555+ffmpeg

---
+++ live555-ffmpeg/ffmpeg-1.2/libavfilter/vf_hqdn3d.d
@@ -0,0 +1,16 @@
+libavfilter/vf_hqdn3d.o: libavfilter/vf_hqdn3d.c config.h \
+ libavutil/common.h libavutil/attributes.h libavutil/version.h \
+ libavutil/avconfig.h libavutil/intmath.h libavutil/arm/intmath.h \
+ libavutil/attributes.h libavutil/common.h libavutil/mem.h \
+ libavutil/error.h libavutil/avutil.h libavutil/mathematics.h \
+ libavutil/rational.h libavutil/intfloat.h libavutil/intfloat_readwrite.h \
+ libavutil/log.h libavutil/pixfmt.h libavutil/version.h \
+ libavutil/old_pix_fmts.h libavutil/internal.h libavutil/timer.h \
+ libavutil/arm/timer.h libavutil/cpu.h libavutil/dict.h libavutil/libm.h \
+ libavutil/pixdesc.h libavutil/intreadwrite.h libavutil/bswap.h \
+ libavutil/arm/bswap.h libavutil/arm/intreadwrite.h \
+ libavfilter/avfilter.h libavutil/avutil.h libavutil/dict.h \
+ libavutil/log.h libavutil/samplefmt.h libavutil/pixfmt.h \
+ libavutil/rational.h libavfilter/version.h libavfilter/formats.h \
+ libavfilter/internal.h libavfilter/avfiltergraph.h libavfilter/video.h \
+ libavfilter/vf_hqdn3d.h
