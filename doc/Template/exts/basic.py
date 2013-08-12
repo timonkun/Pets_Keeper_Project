b8b7981a9ee4376a393f7296940c64509ac627c7
ganling <ganling@pset.suntec.net>
2013-08-12 19:56:33 +0800

    remove Template

--- doc/Template/exts/basic.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import sphinx
-
-def setup(app):
-    app.add_javascript('booktools.js')
--- doc/Template/exts/nohighlight.py
+++
@@ -1,4 +0,0 @@
-from sphinx import highlighting
-def highlight_block(self, source, lang, linenos=False, warn=None):
-    return self.unhighlighted(source)
-highlighting.PygmentsBridge.highlight_block = highlight_block
